856184f708688c0076c74f8634121179bdbd3cdc
_co_cy_ <stf2003.kem@gmail.com>
2022-11-10 06:04:06 +0300

    [develop] -> Added add page

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -1,4 +1,4 @@
-from . import index, login, logout, register, send
+from . import index, login, logout, register, send, add
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -11,3 +11,4 @@ def include_routers(app: 'FastAPI'):
     app.include_router(logout.router)
     app.include_router(register.router)
     app.include_router(send.router, prefix="/user")
+    app.include_router(add.router, prefix="/user")
---
+++ backend/routers/add.py
@@ -0,0 +1,23 @@
+from fastapi.responses import HTMLResponse, RedirectResponse
+from backend.database import AsyncSession, get_session
+from fastapi import APIRouter, Request, Depends, Form
+from backend.database.tables.users import User
+from backend.templates import get_templates
+from backend.auth import get_manager
+from sqlalchemy.future import select
+
+router = APIRouter()
+
+
+@router.get("/add", response_class=HTMLResponse)
+def get_send(request: Request, user=Depends(get_manager())):
+    return get_templates().TemplateResponse("add.html", context={"request": request, 'user': user, "Title": "Пополнить", "page": 1})
+
+
+@router.post("/add")
+async def post_send(request: Request, db_session: AsyncSession = Depends(get_session), user=Depends(get_manager()), count: float = Form(), commission: float = Form()):
+    user.balance_rub += count
+
+    db_session.add(user)
+    await db_session.commit()
+    return get_templates().TemplateResponse("balance.html", context={"request": request, "user": user, "Title": "Баланс", "page": 0})
